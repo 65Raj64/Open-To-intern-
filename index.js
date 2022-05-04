30eb9fb0722d4ba0028bbf8380d16ed3466739a2
saurabh2160 <saurabhmanohar90@gmail.com>
2022-05-04 13:27:21 +0530

    structure

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./route/route');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://saurabh042160:iafSq7ML1zCfugKI@cluster1.ymdh1.mongodb.net/group-58XDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDB_connected"))
+    .catch(err => console.log(err))
+app.use('/', route);
+
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 4000))
+});
+
